de5d8c5814c77226617699d00d63906ab9cd03b9
Ricard Martin Benito <32068910+rmartinbeni@users.noreply.github.com>
2026-02-14 00:02:10 +0100

    Untrack production

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  supabase: {
-    url: 'https://fdipttpljkcnykzjwlew.supabase.co',
-    key: 'sb_publishable_97NAR2hqjAgsY1OiMsaWHw_igNpsS9P',
-  },
-};
